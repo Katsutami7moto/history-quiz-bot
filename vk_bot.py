f98b48cfcf9ccf30867636992fa99747b32459e7
Katsutami7moto <nclsf87@gmail.com>
2022-09-06 12:23:52 +0300

    Add keyboard to VK bot

--- vk_bot.py
+++ vk_bot.py
@@ -1,19 +1,32 @@
 import logging
-import random
 
 from environs import Env
 
 import vk_api
 from vk_api.longpoll import VkLongPoll, VkEventType
+from vk_api.keyboard import VkKeyboard, VkKeyboardColor
+from vk_api.utils import get_random_id
 
 logger = logging.getLogger(__name__)
 
 
-def echo(event, vk_api_method):
-    vk_api_method.messages.send(
-        user_id=event.user_id,
+def get_keyboard():
+    keyboard = VkKeyboard(one_time=True)
+
+    keyboard.add_button('Новый вопрос', color=VkKeyboardColor.PRIMARY)
+    keyboard.add_button('Сдаться', color=VkKeyboardColor.NEGATIVE)
+    keyboard.add_line()
+    keyboard.add_button('Мой счёт', color=VkKeyboardColor.SECONDARY)
+
+    return keyboard.get_keyboard()
+
+
+def echo(event, vk):
+    vk.messages.send(
+        peer_id=event.user_id,
         message=event.text,
-        random_id=random.randint(1,1000)
+        random_id=get_random_id(),
+        keyboard=get_keyboard()
     )
 
 
@@ -28,14 +41,14 @@ def main():
     )
 
     vk_session = vk_api.VkApi(token=vk_club_token)
-    vk_api_method = vk_session.get_api()
+    vk = vk_session.get_api()
     longpoll = VkLongPoll(vk_session)
 
     logger.info('Bot is running.')
 
     for event in longpoll.listen():
         if event.type == VkEventType.MESSAGE_NEW and event.to_me:
-            echo(event, vk_api_method)
+            echo(event, vk)
 
 
 if __name__ == '__main__':
